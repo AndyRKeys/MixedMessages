f9724ff97a47fe42062c881ab378fff2056fb6d0
= <=>
2021-10-26 18:54:00 +0100

    added three data arrays

---
+++ main.js
@@ -0,0 +1,40 @@
+{
+  //  create intial list of character names
+
+  const names = [
+    "Akibrus",
+    "Angun",
+    "Balrus",
+    "Bulruk",
+    "Caldor",
+    "Aleera",
+    "Alva",
+    "Amara",
+    "Anya",
+    "Asralyn",
+    "Azura",
+    "Ailmar",
+    "Akkar",
+    "Axil",
+    "Belstram",
+    "Alea",
+    "Azariah",
+    "Bellas",
+    "Bonna",
+    "Chandrelle",
+    "Angor",
+    "Austi",
+    "Bragi",
+    "Braka",
+    "Amerstal",
+    "Aqrilla",
+    "Bellesta",
+    "Bonnwynn",
+  ];
+
+  const classes = ["warrior","paladin","druid","archer","mage"];
+
+  const races = ["human", "dwarf", "high elf", "dark elf", "goblin"];
+
+
+}
